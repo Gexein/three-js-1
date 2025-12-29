718120ef46aa26f98fbea2a9d4dfa951bb301ab9
Gexein <gexein@yandex.ru>
2025-12-29 16:40:02 +0300

    added shawarma glb, new build

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,31 @@
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, useGLTF } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
 
+useGLTF.preload("/models/shaurma.glb");
 function App() {
+	const { scene } = useGLTF("../../public/3dmodels/untitled.glb");
+
 	return (
 		<>
 			<Canvas camera={{ position: [3, 3, 3] }}>
-				<color attach="background" args={["gold"]} />
-				<OrbitControls />
-				<mesh>
-					<boxGeometry args={[0.5, 0.5, 0.5]} />
-					<meshNormalMaterial />
-				</mesh>
+				<color attach="background" args={["#1a1717"]} />
+				<OrbitControls
+					target={[1, 1, 0]}
+					enablePan={true}
+					enableZoom={true}
+					enableRotate={true}
+				/>
+
+				<Suspense fallback={null}>
+					<primitive
+						object={scene}
+						position={[1, 1, 0]}
+						scale={[0.5, 0.5, 0.5]}
+					/>
+				</Suspense>
+				<ambientLight intensity={0.7} />
+				<pointLight position={[10, 10, 10]} intensity={1} />
 			</Canvas>
 		</>
 	);
